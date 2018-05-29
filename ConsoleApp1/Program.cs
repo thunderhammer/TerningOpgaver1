711481688a8d0447f332cde361b26ff693fc24e1
Morten Engemand <engemand@hotmail.com>
2018-05-29 11:36:39 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArvRandom
+{
+
+    class Program
+    {
+        private static UdvidetRandom r = new UdvidetRandom();
+
+        static void Main(string[] args)
+        {      
+            Console.WriteLine(r.NextBool());
+            
+        }
+
+    }
+
+    class UdvidetRandom : Random
+    {
+        public (bool,int) NextBool()
+        {
+            int number = 0;
+            bool result = false;
+
+            number = this.Next(1, 1002);
+
+            if (number < 500) result = true;
+            else result = false;
+
+
+
+            return (result,number);
+        }
+
+
+    }
+}
